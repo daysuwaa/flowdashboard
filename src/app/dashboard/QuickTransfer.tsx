5e9fa480af11e2c2cad831f6d945c9a7c9406c97
Aiwerioghene <daysuwaaiwerioghene@icloud.com>
2025-07-17 15:27:18 +0100

    fixes

--- src/app/dashboard/QuickTransfer.tsx
+++ src/app/dashboard/QuickTransfer.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Images_titles from './QuickTransfer/Personnels'
+import BalancePage from './Balance/Balance'
 
-const QuickTransfer = () => {
+const Qt = () => {
   return (
-    <div>QuickTransfer</div>
+    <div className='mt-6'>
+    <div className="grid xl:grid-cols-3 gap-10 h-full">
+  <Images_titles />
+  <BalancePage />
+</div>
+    </div>
   )
 }
 
-export default QuickTransfer
+export default Qt
